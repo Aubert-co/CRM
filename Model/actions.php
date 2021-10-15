5efae974ddcb1b3c4f05bd5aa58770d33845a6e5
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-14 23:21:50 -0300

    Update actions.php

---
+++ Model/actions.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App\Model;
+
+use App\Model\GetConection;
+
+class Actions {
+    public function Insert_Datas()
+    {
+
+    }
+
+    public function Select_Datas()
+    {
+
+    }
+    public function Find_Dates($Date_Start, $Date_End){
+       $sql =  "SELECT * FROM data_users where visit_date BETWEEN '{$Date_Start}'  AND '{$Date_End}'";
+    }
+
+    public function Find_Global($Data){
+
+    }
+}
+
+?>
