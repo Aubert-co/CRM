b1a84c573b15d27bb64096397a0b808bf96f2833
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-18 20:24:14 -0300

    Create header.php

---
+++ App/View/header.php
@@ -0,0 +1,21 @@
+<header>
+        <div class="flex_nav">
+               <div class="box_inputs">
+                   <h3>Faça uma busca</h3>
+                    <select name="" id="selectValues">
+                        <option value="street">Bairro</option>
+                        <option value="date">Data</option>
+                        <option value="employeName">Vendedor</option>
+                        <option value="person">Pessoa ou Empressa</option>
+                    </select>
+                    <input type="text" id="inputSearchValues" placeholder="faça uma busca formato">
+                </div>
+
+            <div class="box_datasSearch">
+                <h3>Faça uma busca por data</h3>
+                De:<input type="text" id="searchDatesFrom" placeholder="DDMMAAAA">
+
+                Até:<input type="text" id="searchDatesTo" placeholder="DDMMAAAA">
+            </div>
+        </div>
+</header>
