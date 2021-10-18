01fcfbff45dac3e2564d3bb4ba5f95a8e04b1518
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-18 20:23:36 -0300

    Update FeedBack_Query.php

--- App/Model/FeedBack_Query.php
+++ App/Model/FeedBack_Query.php
@@ -3,40 +3,57 @@
 namespace App\Model;
 require_once '../vendor/autoload.php';
 
-
-
 use App\Model\GetConection;
 
 
 class FeedBack_Query{
     public function Select_FeedBack_Positivo($order,$search){
-        $feedBack = 3;
+        $feedBack = 1;
         $sql =  $this->Return_a_SQL($order,$search,$feedBack);
-         print($sql);
+        
         $stmt = GetConection::Conection()->prepare($sql);
  
         $stmt->execute();
  
         if($stmt->rowCount()>0){
             $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
- 
-            foreach($result as $values){
-             print($values['name_user']);
-            }
+            
+            return $result;
         }
+   
+     
      }
      public function Select_FeedBack_Negative($order,$search){
         $feedBack = 1;
         
-        $sql = $this->Return_a_SQL($order,$search,$feedBack);
+        $sql =  $this->Return_a_SQL($order,$search,$feedBack);
+         
         $stmt = GetConection::Conection()->prepare($sql);
-
+ 
         $stmt->execute();
+ 
+        if($stmt->rowCount()>0){
+            $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            
+            return $result;
+        }
         
      }
 
      public function Select_FeedBack_Medium($order,$search){
-         $feedBack = 2;
+        
+         $feedBack = 1;
+         $sql =  $this->Return_a_SQL($order,$search,$feedBack);
+         
+         $stmt = GetConection::Conection()->prepare($sql);
+  
+         $stmt->execute();
+  
+         if($stmt->rowCount()>0){
+             $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+             
+             return $result;
+         }
      }
 
  
@@ -55,6 +72,46 @@ class FeedBack_Query{
          return $sql = "SELECT * FROM data_users where feedback = '{$feedBack}' ";
      }
 }
+/*function FeedBack_Layout_Medium($order,$search,$feedback){
+    $Values_Query = new FeedBack_Query;
+    $Datas = $Values_Query->Select_FeedBack_Positivo(null,null);
+  
+    $Layout ='
+    <div class="box_itens">
 
-print('ola');
+                <div class="title">
+                    <div class="title">
+                        <div class="title_text">
+                            <h6>Maiores chances de contratar</h6>
+                        </div>
+                        
+                        <div class="title_number">
+                            <h3>5</h3>
+                        </div>
+                        
+                        <div class="title_input">
+                            <input type="text" placeholder="faça uma busca">
+                        </div>
+                        </div>
+  ';
+    foreach($Datas as $values){
+        $Layout .='
+        <div class="sub_items">
+        <div class="item_box hight">
+            <h4>'.$values['name_user'].'</h4>
+            <h4>'.$values['name_employe'].'</h4>
+            <h4>'.$values['address_user'].'</h4>
+            <h4>'.$values['contact_user'].' </h4>
+            <h4>'.$values['feedback'].'</h4>
+            <h4>'.$values['visit_date'].'</h4>
+        </div>
+
+        </div>
+        ';
+        $Layout .=' </div>';
+        
+    }
+    return $Layout;
+}*/
+
 ?>
