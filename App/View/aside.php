fd14fd38f85ade023182ce5ee9c18fa89cb1c484
Black <53127242+Aubert-co@users.noreply.github.com>
2021-10-18 20:23:54 -0300

    Create aside.php

---
+++ App/View/aside.php
@@ -0,0 +1,20 @@
+<aside>
+        <div class="flex_aside">
+            
+                <a href="#">
+                    <i class="material-icons" title="Editar">mode_edit</i>
+                    Editar
+                </a>
+            
+                <a href="#" title="Dashboard">
+                    <i class="material-icons">dashboard</i>
+                    Dashboard
+                </a>
+        
+                <a href="#" class="sub_item" >
+                    <i class="material-icons" class="sub_item"  title="Ajuda">help</i>
+                    Ajuda
+                </a>
+            
+        </div>
+</aside>
